0303bef309c035b55ebbb61a59b1dc0c566acc84
Monika Bole <venkatamonikabole@gmail.com>
2025-08-04 17:13:57 +0530

    Update: explicit command injection example

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -1,6 +1,8 @@
 import os
 
-def run_command(user_input):
-    os.system("echo " + user_input)
+def run_command():
+    user_input = input("Enter your command: ")
+    # CWE-78: Command Injection
+    os.system(user_input)  # 🚨 Dangerous use of raw input
 
-run_command(input("Enter something: "))
+run_command()
