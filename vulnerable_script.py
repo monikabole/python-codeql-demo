c5f0495b878729427227379027e4a4fbe695dcc1
monikabole <venkatamonikabole@gmail.com>
2025-08-04 19:43:50 +0530

    Update vulnerable_script.py

--- vulnerable_script.py
+++ vulnerable_script.py
@@ -1,24 +1,12 @@
 import os
 
-# CWE-78: Command Injection
-def run_command():
-    user_input = input("Enter shell command: ")
-    os.system(user_input)
+# Hardcoded password
+username = "admin"
+password = "123456"
 
-run_command()
+# Unsafe command execution
+os.system("rm -rf /")  # Dangerous!
 
-# CWE-259: Hardcoded Credentials
-def insecure_login():
-    password = "supersecret"
-    user_input = input("Password: ")
-    if user_input == password:
-        print("Logged in")
-
-insecure_login()
-
-# CWE-94: Eval Injection
-def code_exec():
-    code = input("Run Python code: ")
-    eval(code)
-
-code_exec()
+# Insecure use of eval()
+user_input = input("Enter something: ")
+eval(user_input)
